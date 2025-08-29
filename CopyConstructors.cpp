d6878f5dfa6accc4b88dc3509d172e19ae0c00dc
Sanjana Anuj Pawar <pawarsanjana2006@gmail.com>
2025-08-28 23:38:35 -0400

    Create CopyConstructors.cpp

---
+++ CopyConstructors.cpp
@@ -0,0 +1,30 @@
+//copy constructor
+#include <iostream>
+#include<string>
+using namespace std;
+class student{
+    int rno;
+    string name;
+    int fee;
+    public:
+    student(int rn,string nm, int fe){
+        rno=rn;
+        name=nm;
+        fee=fe;
+    }
+    student(student&t){
+        rno=t.rno;
+        name=t.name;
+        fee=t.fee;
+    }
+    void display(){
+       cout<<endl<<rno<<"\t"<<name<<"\t"<<fee;
+    }
+};
+int main() {
+   student s(15,"student1",10000);
+   s.display();
+   student Student1(s);//copy constructor called
+   Student1.display();
+    return 0;
+}
